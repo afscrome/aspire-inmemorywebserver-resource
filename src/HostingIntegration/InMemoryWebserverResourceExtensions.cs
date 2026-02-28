a5de2c66f6c15c277ebabdd5bef2e0757d77aab1
afscrome <afscrome@users.noreply.github.com>
2026-02-28 21:30:14 +0000

    Refactor resource initialisation to slightly break away the specifics of the web server implementation.

--- src/HostingIntegration/InMemoryWebserverResourceExtensions.cs
+++ src/HostingIntegration/InMemoryWebserverResourceExtensions.cs
@@ -1,5 +1,6 @@
 using Aspire.Hosting;
 using Aspire.Hosting.ApplicationModel;
+using Aspire.Hosting.Eventing;
 using Aspire.Hosting.InMemoryWebServer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -34,179 +35,84 @@ public static class InMemoryWebserverResourceExtensions
                 State = KnownResourceStates.NotStarted,
                 Properties = [
                     new(CustomResourceKnownProperties.Source, ".")
-                ]
+                ],
             })
             .WithOtlpExporter()
             .OnInitializeResource(Initialise)
             .WithHttpsEndpoint();
 
-
         async Task Initialise(InMemoryWebserverResource resource, InitializeResourceEvent evt, CancellationToken ct)
         {
-            var endpoint = resource.GetEndpoint("https");
-            var endpointAnnotation = endpoint.EndpointAnnotation;
-
-            // Publish BeforeStart event.  This will block if Waits have been configured
-            await evt.Eventing.PublishAsync(new BeforeResourceStartedEvent(resource, evt.Services), ct);
-
-            var executionConfiguration = await GetExecutionConfig();
-
-
-            await evt.Notifications.PublishUpdateAsync(resource, s => s with
-            {
-                StartTimeStamp = DateTime.UtcNow,
-                State = KnownResourceStates.Starting,
-                EnvironmentVariables = executionConfiguration.EnvironmentVariables.Select(x => new EnvironmentVariableSnapshot(x.Key, x.Value, true)).ToImmutableArray(),
-                Properties = SetArgumentProperties(s.Properties, executionConfiguration)
-            });
-
-            async Task<IExecutionConfigurationResult> GetExecutionConfig()
-            {
-                var executionConfigurationBuilder = ExecutionConfigurationBuilder.Create(resource)
-                    .WithEnvironmentVariablesConfig()
-                    .WithArgumentsConfig();
-
-
-                var executionContext = evt.Services.GetRequiredService<DistributedApplicationExecutionContext>();
-                return await executionConfigurationBuilder.BuildAsync(executionContext, evt.Logger, ct);
-            }
-
-            static ImmutableArray<ResourcePropertySnapshot> SetArgumentProperties(
-                ImmutableArray<ResourcePropertySnapshot> existing,
-                IExecutionConfigurationResult configurationResult)
-            {
-                // From internal  `KnownProperties.Resource` strings
-                const string AppArgsPropertyName = "resource.appArgs";
-                const string AppArgsSensitivityPropertyName = "resource.appArgsSensitivity";
-
-                var args = configurationResult.Arguments.Select(x => x.Value).ToImmutableArray();
-                var argSensitivity = configurationResult.Arguments.Select(x => Convert.ToInt32(x.IsSensitive)).ToImmutableArray();
-                bool isSensitive = configurationResult.Arguments.Any(x => x.IsSensitive);
-
-                return [
-                    .. existing.Where(p => p.Name is not AppArgsPropertyName and not AppArgsSensitivityPropertyName),
-                    new ResourcePropertySnapshot(AppArgsPropertyName, args) { IsSensitive = isSensitive },
-                    new ResourcePropertySnapshot(AppArgsSensitivityPropertyName, argSensitivity)
-                ];
-            }
-
-            async Task<WebApplicationBuilder> CreateInitialBuilder()
-            {
-                var builder = WebApplication.CreateBuilder();
-
-                ConfigureConfiguration();
-                ConfigureLogging();
-                ConfigureOtel();
-
-                //TODO: Configure TLs based on certs?
-                await ConfigureUrls(builder, endpointAnnotation, ct);
-
-                return builder;
-
-                void ConfigureConfiguration()
-                {
-                    var args = executionConfiguration.Arguments.Select(x => x.Value).ToArray();
-                    var envVars = executionConfiguration.EnvironmentVariables.ToDictionary(x => x.Key, x => (string?)x.Value);
-                    builder.Configuration
-                        .AddCommandLine(args)
-                        .AddInMemoryCollection(envVars);                    
-                }
-
-                void ConfigureLogging()
-                {
-                    builder.Services.AddSingleton(evt.Services.GetRequiredService<IConfigureOptions<LoggerFilterOptions>>());
-                    builder.Logging.AddProvider(new ForwardingLoggerProvider(evt.Logger));
-                }
-
-                void ConfigureOtel()
-                {
-                    if (resource.TryGetAnnotationsOfType<OtlpExporterAnnotation>(out var otlpAnnotation))
-                    {
-                        builder.Services
-                            .AddOpenTelemetry()
-                            .UseOtlpExporter();
-                    }
-                }
-
-                async Task ConfigureUrls(WebApplicationBuilder builder, EndpointAnnotation endpointAnnotation, CancellationToken ct)
-                {
-                    builder.Configuration["ASPNETCORE_PREFERHOSTINGURLS"] = "true";
-
-                    var targetAddress = endpointAnnotation.TargetHost;
-
-                    var dnsEntry = await Dns.GetHostEntryAsync(endpointAnnotation.TargetHost, ct);
-                    //if entry is localhost
-                    if (dnsEntry.AddressList.Any(x => x.Equals(IPAddress.Loopback)))
-                    {
-                        targetAddress = IPAddress.Loopback.ToString();
-                    }
-                    else if (dnsEntry.AddressList.Any(x => x.Equals(IPAddress.IPv6Loopback)))
-                    {
-                        targetAddress = IPAddress.IPv6Loopback.ToString();
-                    }
-
-                    var url = new UriBuilder
-                    {
-                        Scheme = endpointAnnotation.UriScheme,
-                        Host = targetAddress,
-                        Port = endpointAnnotation.TargetPort ?? 0
-                    }.ToString();
-
-                    builder.WebHost.UseUrls(url);
-                    if (endpointAnnotation.UriScheme == "https")
-                    {
-                        builder.WebHost.UseKestrelHttpsConfiguration();
-                    }
-                }
-            }
-
-            //TODO: dispose properly, add start /stop commands
-            WebApplication app;
+            WebApplication? app = default;
             try
             {
-                var builder = await CreateInitialBuilder();
-                app = await configure(builder);
+                var endpoint = resource.GetEndpoint("https");
+                var endpointAnnotation = endpoint.EndpointAnnotation;
+                var executionContext = evt.Services.GetRequiredService<DistributedApplicationExecutionContext>();
+
+                // Publish BeforeStart event.  This will block if Waits have been configured
+                await evt.Eventing.PublishAsync(new BeforeResourceStartedEvent(resource, evt.Services), ct);
+
+                var executionConfiguration = await BuildExecutionConfig(resource, executionContext, evt.Logger, ct);
+
+                await evt.Notifications.PublishUpdateAsync(resource, s => s with
+                {
+                    StartTimeStamp = DateTime.UtcNow,
+                    State = KnownResourceStates.Starting,
+                    EnvironmentVariables = [.. executionConfiguration.EnvironmentVariables.Select(x => new EnvironmentVariableSnapshot(x.Key, x.Value, true))],
+                    Properties = SetArgumentProperties(s.Properties, executionConfiguration)
+                });
+
+                app = await BuildWebApp(evt, configure, executionConfiguration, endpointAnnotation, ct);
 
                 await app.StartAsync(ct);
 
-                AllocateEndpoint(app, endpointAnnotation, evt.Services);
-                await evt.Eventing.PublishAsync(new ResourceEndpointsAllocatedEvent(resource, evt.Services), ct);
+                await AllocateEndpoints(app, endpointAnnotation, evt, ct);
 
                 await evt.Notifications.PublishUpdateAsync(resource, x => x with
                 {
                     State = KnownResourceStates.Running,
                     StartTimeStamp = DateTime.UtcNow,
-                    Urls = [
-                        ..x.Urls,
-                        new("http", endpoint.Url, false)
-                    ],
-
                 });
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                evt.Logger.LogError(ex, "Failed to start InMemoryWebServer resource");
-                // Transition to failed to start
+                if (!ct.IsCancellationRequested)
+                {
+                    evt.Logger.LogError(ex, "Failed to start InMemoryWebServer resource");
+                }
+
+                if (app is not null)
+                {
+                    await app.DisposeAsync();
+                }
+
                 await evt.Notifications.PublishUpdateAsync(resource, x => x with
                 {
                     State = KnownResourceStates.FailedToStart,
-                    StopTimeStamp = DateTime.UtcNow
+                    StopTimeStamp = DateTime.UtcNow,
                 });
+
                 throw;
             }
+
 
             int exitCode = 0;
             try
             {
+                //TODO: A better delay
+                await Task.Delay(Timeout.Infinite, ct);
                 await app.WaitForShutdownAsync(ct);
             }
             catch
             {
-                exitCode = 1;
+                exitCode = ct.IsCancellationRequested ? 0 : 1;
                 throw;
             }
             finally
             {
+                await app.DisposeAsync();
+
                 await evt.Notifications.PublishUpdateAsync(resource, x => x with
                 {
                     State = KnownResourceStates.Finished,
@@ -215,44 +121,154 @@ public static class InMemoryWebserverResourceExtensions
                 });
             }
         }
+    }
 
-        static void AllocateEndpoint(WebApplication app, EndpointAnnotation endpointAnnotation, IServiceProvider services)
+    static async Task<WebApplication> BuildWebApp(InitializeResourceEvent evt, Func<WebApplicationBuilder, Task<WebApplication>> configure, IExecutionConfigurationResult executionConfigurationResult, EndpointAnnotation endpointAnnotation, CancellationToken ct)
+    {
+        var builder = WebApplication.CreateBuilder();
+
+        ConfigureConfiguration();
+        ConfigureLogging();
+        ConfigureOtel();
+
+        //TODO: Configure TLs based on certs?
+        await ConfigureUrls(builder, endpointAnnotation, ct);
+
+        var app = await configure(builder);
+        return app;
+
+        void ConfigureConfiguration()
         {
-            var server = app.Services.GetRequiredService<IServer>();
-            var addressFeature = server.Features.GetRequiredFeature<IServerAddressesFeature>();
-            var actualAddress = addressFeature.Addresses.First();
+            string[] args = [.. executionConfigurationResult.Arguments.Select(x => x.Value)];
+            var envVars = executionConfigurationResult.EnvironmentVariables.ToDictionary(x => x.Key, x => (string?)x.Value);
+            builder.Configuration
+                .AddCommandLine(args)
+                .AddInMemoryCollection(envVars);
+        }
 
-            var actualUri = new Uri(actualAddress);
+        void ConfigureLogging()
+        {
+            builder.Services.AddSingleton(evt.Services.GetRequiredService<IConfigureOptions<LoggerFilterOptions>>());
+            builder.Logging.AddProvider(new ForwardingLoggerProvider(evt.Logger));
+        }
 
-
-            var hostEndpoint = new AllocatedEndpoint(endpointAnnotation, actualUri.Host, actualUri.Port, EndpointBindingMode.SingleAddress, networkID: KnownNetworkIdentifiers.LocalhostNetwork);
-            endpointAnnotation.AllocatedEndpointSnapshot.SetValue(hostEndpoint);
-
-            var containerHostName = GetContainerHostName(services);
-
-            var containerAllocatedEndpoint = new AllocatedEndpoint(endpointAnnotation, containerHostName, actualUri.Port, EndpointBindingMode.SingleAddress, networkID: KnownNetworkIdentifiers.DefaultAspireContainerNetwork);
-            var snapshot = new ValueSnapshot<AllocatedEndpoint>();
-            snapshot.SetValue(containerAllocatedEndpoint);
-
-            endpointAnnotation.AllAllocatedEndpoints.TryAdd(KnownNetworkIdentifiers.DefaultAspireContainerNetwork, snapshot);
-
-
-            // This is ugly, but the container name is derrived from several possible options, some of which are non public (_dcpInfo)
-            // so I can't even replicate the checks against IConfiguration myself 
-            static string GetContainerHostName(IServiceProvider services)
+        void ConfigureOtel()
+        {
+            if (evt.Resource.TryGetAnnotationsOfType<OtlpExporterAnnotation>(out var otlpAnnotation))
             {
-                var dcpExecutorType = Type.GetType("Aspire.Hosting.Dcp.IDcpExecutor, Aspire.Hosting")
-                    ?? throw new InvalidOperationException("Failed to get DcpExecutor");
-
-                var dcpExecutor = services.GetRequiredService(dcpExecutorType);
-
-                var hostNameMethod = dcpExecutor.GetType().GetMethod("get_ContainerHostName", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
-                    ?? throw new InvalidOperationException("Failed to get ContainerHostName from DcpExecutor");
-
-                return (string)hostNameMethod.Invoke(dcpExecutor, [])!;
+                builder.Services
+                    .AddOpenTelemetry()
+                    .UseOtlpExporter();
             }
         }
 
-    }
-}
+        async Task ConfigureUrls(WebApplicationBuilder builder, EndpointAnnotation endpointAnnotation, CancellationToken ct)
+        {
+            builder.Configuration["ASPNETCORE_PREFERHOSTINGURLS"] = "true";
 
+            var targetAddress = endpointAnnotation.TargetHost;
+
+            var dnsEntry = await Dns.GetHostEntryAsync(endpointAnnotation.TargetHost, ct);
+            //if entry is localhost
+            if (dnsEntry.AddressList.Any(x => x.Equals(IPAddress.Loopback)))
+            {
+                targetAddress = IPAddress.Loopback.ToString();
+            }
+            else if (dnsEntry.AddressList.Any(x => x.Equals(IPAddress.IPv6Loopback)))
+            {
+                targetAddress = IPAddress.IPv6Loopback.ToString();
+            }
+
+            var url = new UriBuilder
+            {
+                Scheme = endpointAnnotation.UriScheme,
+                Host = targetAddress,
+                Port = endpointAnnotation.TargetPort ?? 0
+            }.ToString();
+
+            builder.WebHost.UseUrls(url);
+            if (endpointAnnotation.UriScheme == "https")
+            {
+                builder.WebHost.UseKestrelHttpsConfiguration();
+            }
+        }
+    }
+
+    static async Task AllocateEndpoints(WebApplication app, EndpointAnnotation endpointAnnotation, InitializeResourceEvent evt, CancellationToken ct)
+    {
+        var server = app.Services.GetRequiredService<IServer>();
+        var addressFeature = server.Features.GetRequiredFeature<IServerAddressesFeature>();
+        var actualAddress = addressFeature.Addresses.First();
+
+        var actualUri = new Uri(actualAddress);
+
+        var hostEndpoint = new AllocatedEndpoint(endpointAnnotation, actualUri.Host, actualUri.Port, EndpointBindingMode.SingleAddress, networkID: KnownNetworkIdentifiers.LocalhostNetwork);
+
+        var containerHostName = GetContainerHostName(evt.Services);
+        var containerAllocatedEndpoint = new AllocatedEndpoint(endpointAnnotation, containerHostName, actualUri.Port, EndpointBindingMode.SingleAddress, networkID: KnownNetworkIdentifiers.DefaultAspireContainerNetwork);
+
+        endpointAnnotation.AllAllocatedEndpoints.AddOrUpdateAllocatedEndpoint(KnownNetworkIdentifiers.LocalhostNetwork, hostEndpoint);
+        endpointAnnotation.AllAllocatedEndpoints.AddOrUpdateAllocatedEndpoint(KnownNetworkIdentifiers.DefaultAspireContainerNetwork, containerAllocatedEndpoint);
+
+        await evt.Eventing.PublishAsync(new ResourceEndpointsAllocatedEvent(evt.Resource, evt.Services), ct);
+        await evt.Notifications.PublishUpdateAsync(evt.Resource, x => x with
+        {
+            Urls = [
+                ..x.Urls,
+                new("http", hostEndpoint.UriString, false)
+            ],
+        });
+
+
+        // This is ugly, but the container name is derived from several possible options, some of which are non public (_dcpInfo)
+        // so I can't even replicate the checks against IConfiguration myself 
+        static string GetContainerHostName(IServiceProvider services)
+        {
+            var dcpExecutorType = Type.GetType("Aspire.Hosting.Dcp.IDcpExecutor, Aspire.Hosting")
+                ?? throw new InvalidOperationException("Failed to get DcpExecutor");
+
+            var dcpExecutor = services.GetRequiredService(dcpExecutorType);
+
+            var hostNameMethod = dcpExecutor.GetType().GetMethod("get_ContainerHostName", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)
+                ?? throw new InvalidOperationException("Failed to get ContainerHostName from DcpExecutor");
+
+            return (string)hostNameMethod.Invoke(dcpExecutor, [])!;
+        }
+    }
+
+    static ImmutableArray<ResourcePropertySnapshot> SetArgumentProperties(
+        ImmutableArray<ResourcePropertySnapshot> existing,
+        IExecutionConfigurationResult configurationResult)
+    {
+        // From internal  `KnownProperties.Resource` strings
+        const string AppArgsPropertyName = "resource.appArgs";
+        const string AppArgsSensitivityPropertyName = "resource.appArgsSensitivity";
+
+        var args = configurationResult.Arguments.Select(x => x.Value).ToImmutableArray();
+        var argSensitivity = configurationResult.Arguments.Select(x => Convert.ToInt32(x.IsSensitive)).ToImmutableArray();
+        bool isSensitive = configurationResult.Arguments.Any(x => x.IsSensitive);
+
+        return [
+            .. existing.Where(p => p.Name is not AppArgsPropertyName and not AppArgsSensitivityPropertyName),
+                new ResourcePropertySnapshot(AppArgsPropertyName, args) { IsSensitive = isSensitive },
+                new ResourcePropertySnapshot(AppArgsSensitivityPropertyName, argSensitivity)
+        ];
+    }
+
+    static async Task<IExecutionConfigurationResult> BuildExecutionConfig(IResource resource, DistributedApplicationExecutionContext executionContext, ILogger logger, CancellationToken ct)
+    {
+        var executionConfigurationBuilder = ExecutionConfigurationBuilder.Create(resource);
+
+        if (resource is IResourceWithArgs)
+        {
+            executionConfigurationBuilder.WithArgumentsConfig();
+        }
+        if (resource is IResourceWithEnvironment)
+        {
+            executionConfigurationBuilder.WithEnvironmentVariablesConfig();
+        }
+
+        return await executionConfigurationBuilder.BuildAsync(executionContext, logger, ct);
+    }
+
+}
